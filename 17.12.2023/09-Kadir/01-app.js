097affa6a021813480765824af46945d7c40b614
KadirWebDev <151651490+KadirWebDev@users.noreply.github.com>
2023-12-22 21:58:09 +0100

    JS 1.hafta kod tekrari

---
+++ 17.12.2023/09-Kadir/01-app.js
@@ -0,0 +1,77 @@
+console.log("Hello World")
+
+
+/*
+    PRIMITIVE TYPES
+    -String
+    -Number
+    -Boolean
+    -Undefined
+    -Null
+*/
+
+/*
+    REFERANCE TYPES
+    -Array
+    -Object
+    -Function
+ */
+
+/* 
+    camelCase = firstName
+    PascalCase = FirstName
+    snake_case = first_name
+    kebab-case = first-name
+*/
+
+/* 
+    STRING METHODLAR
+    String length
+    String slice()
+    String replace()
+    String toUpperCase()
+    String toLowerCase()
+    String concat()
+    String trim()
+    String trimStart()
+    String trimEnd()
+    String charAt()
+    String split()
+*/
+
+/*
+    OPERATORLER
+    -Matematiksel
+        +
+        -
+        *
+        ** 
+        /
+        %
+        =
+        +=
+        -=
+        *=
+        /=
+        %=
+        **= 
+        ++
+        --
+
+
+    -Karsilastirma
+        ==
+        ===
+        !=
+        >
+        <
+        >=
+        <=
+
+
+    -Mantiksal
+        &&
+        ||
+        !
+
+*/
